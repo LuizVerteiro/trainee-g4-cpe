802a4ea5a737c4e5c0605122724c34cf82936c9d
Isabel Assuncao <isabelassuncao9@gmail.com>
2023-11-02 18:07:42 -0300

    Header

--- trainee-cpe/src/Pages/Home/Home.jsx
+++ trainee-cpe/src/Pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import { Container } from "./Styles";
+import { DivHeader } from "../Cadastro/Styles";
+import { DivLogo } from "../Cadastro/Styles";
+import { DivText } from "../Cadastro/Styles";
 
 function Home() {
   return (
     <Container>
-      <div>Essa é a página home</div>
+      <DivHeader>
+        <DivLogo>
+          <DivText>Essa e a pagina home</DivText>
+        </DivLogo>
+      </DivHeader>
     </Container>
   );
 }
