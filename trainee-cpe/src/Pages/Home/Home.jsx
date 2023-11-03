b076e8d2059e3d99fcea5c822dcbc2251bf0aef5
Lara de Oliveira <laraffolivera@gmail.com>
2023-11-03 19:08:03 -0300

    Arrumei a home

--- trainee-cpe/src/Pages/Home/Home.jsx
+++ trainee-cpe/src/Pages/Home/Home.jsx
@@ -1,9 +1,9 @@
-import { Container } from "./Styles";
+import { Container, Firstpart, ButtonEntrar, Title, SecondPart, Title2, ButtonCadastro } from "./Styles";
 import { DivHeader } from "../Cadastro/Styles";
 import { DivLogo } from "../Cadastro/Styles";
-import { DivText } from "./Styles";
 
 function Home() {
+<<<<<<< Updated upstream
   return <div>Ola</div>;
 }
 export default Home;
@@ -17,3 +17,19 @@ export default Home;
         </DivLogo>
       </DivHeader>
     </Container>*/
+=======
+  return (
+    <Container>
+      <Firstpart>
+        <Title> Já tem uma conta?</Title>
+        <ButtonEntrar>Entrar</ButtonEntrar>
+      </Firstpart>
+      <SecondPart>
+        <Title2>Ou cadastre-se:</Title2>
+        <ButtonCadastro>Cadastre-se</ButtonCadastro>
+      </SecondPart>
+    </Container>
+  );
+}
+export default Home;
+>>>>>>> Stashed changes
--- trainee-cpe/src/Pages/Home/Styles.js
+++ trainee-cpe/src/Pages/Home/Styles.js
@@ -17,3 +17,35 @@ export const DivText = styled.div`
   font-weight: 700;
   color: #fff;
 `;
+export const ButtonEntrar = styled.div`
+
+  background: #D32E00;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
+  text-align:center;
+  padding: 2px;
+`;
+export const Title = styled.div`
+padding: 2px;
+
+`;
+export const Firstpart = styled.div`
+  justify-content: center;
+  align-items: center;
+  display:flex;
+  flex-direction: column;
+`;
+export const SecondPart = styled.div`
+justify-content: center;
+  align-items: center;
+  display:flex;
+  flex-direction: column;
+`;
+export const ButtonCadastro = styled.div`
+background: #D32E00;
+box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
+text-align:center;
+padding: 2px;
+`;
+export const Title2 = styled.div`
+  padding: 2px;
+`;
