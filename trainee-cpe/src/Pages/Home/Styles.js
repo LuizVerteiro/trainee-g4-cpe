f70573cd18582ecf925d9f5225d2606542bee145
Lara de Oliveira <laraffolivera@gmail.com>
2023-11-06 23:44:58 -0300

    Arrumando a home

--- trainee-cpe/src/Pages/Home/Styles.js
+++ trainee-cpe/src/Pages/Home/Styles.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: black;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
   height: 80rem;
   color: white;
 `;
@@ -12,7 +13,7 @@ export const DivText = styled.div`
   padding-left: 0px;
   margin-top: 0px;
   font-family: "Monteserrat";
-  font-size: 64px;
+  font-size: 100px;
   font-style: normal;
   font-weight: 700;
   color: #fff;
@@ -23,15 +24,28 @@ export const ButtonEntrar = styled.div`
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
   text-align:center;
   padding: 2px;
+  font-size: 50px;
+  display: flex;
+  width: 200px;
+  height: 64px;
+  padding: 10px 39px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
 `;
 export const Title = styled.div`
-padding: 2px;
-
+padding: 200px;
+font-family: Montserrat;
+font-size: 64px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
 `;
 export const Firstpart = styled.div`
   justify-content: center;
   align-items: center;
   display:flex;
+  margin-right:50px;
   flex-direction: column;
 `;
 export const SecondPart = styled.div`
@@ -45,7 +59,20 @@ background: #D32E00;
 box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
 text-align:center;
 padding: 2px;
+font-size: 50px;
+display: flex;
+width: 344px;
+height: 64px;
+padding: 10px 36px 10px 39px;
+justify-content: center;
+align-items: center;
 `;
 export const Title2 = styled.div`
-  padding: 2px;
+  padding: 200px;
+  color: #FFF;
+  font-family: Montserrat;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 `;
