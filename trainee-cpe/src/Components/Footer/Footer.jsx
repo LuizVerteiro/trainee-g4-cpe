873831b3647fbc741c38c383360566ff1441fd58
Isabel Assuncao <isabelassuncao9@gmail.com>
2023-11-07 15:12:55 -0300

    Inicio Footer

---
+++ trainee-cpe/src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import { DivBackground, Texto } from "./Styles";
+
+function Footer() {
+  return (
+    <DivBackground>
+      <Texto>Assistec 2023</Texto>
+    </DivBackground>
+  );
+}
+
+export default Footer;
---
+++ trainee-cpe/src/Components/Footer/Styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const DivBackground = styled.div`
+  background: linear-gradient(180deg, rgba(222, 49, 0, 0) 0%, #de3100 27.27%);
+  width: 100%;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Texto = styled.div`
+  display: flex;
+  align-items: center;
+  color: #fff;
+  text-align: center;
+  font-family: Montserrat;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
