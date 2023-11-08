d9cf24767a4dba36f4dfc3c4416e8acbb5f5c50d
Lara de Oliveira <laraffolivera@gmail.com>
2023-11-07 23:16:13 -0300

    mudando

--- trainee-cpe/src/Pages/Home/Styles.js
+++ trainee-cpe/src/Pages/Home/Styles.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  flex-wrap: wrap;
   background-color: black;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
   height: 80rem;
   color: white;
@@ -19,84 +20,83 @@ export const DivText = styled.div`
   color: #fff;
 `;
 export const ButtonEntrar = styled.div`
-
-  background: #D32E00;
+  background: #d32e00;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
-  text-align:center;
+  text-align: center;
   padding: 2px;
   font-size: 50px;
   display: flex;
-  max-width: 200px;
+  max-width: 300px;
   height: auto;
-  padding: 10px 10px 10px 10px;
+  padding: 8px 8px 8px 8px;
   justify-content: center;
   align-items: center;
-  @media (max-width:1200px){
-  width: 100%;
-  height: auto;
-  font-size: 30px;
-  padding: 12px 12px 12px 12px;
- };
+  @media (max-width: 1200px) {
+    width: 100%;
+    height: auto;
+    font-size: 40px;
+    padding: 10px 10px 10px 10px;
+  }
 `;
 export const Title = styled.div`
-color: #FFF;
-white-space: nowrap;
-padding: 100px;
-font-family: Montserrat;
-font-size: 40px;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
- @media (max-width:1200px){
-  font-size: 25px;
- };
-`;
-export const Firstpart = styled.div`
-  width: 50%;
-  justify-content: center;
-  align-items: center;
-  display:flex;
-  margin-right: 2%;
-  flex-direction: column;
-  @media (max-width:1500px){
- };
-`;
-export const SecondPart = styled.div`
-justify-content: center;
-  align-items: center;
-  display:flex;
-  flex-direction: column;
-`;
-export const ButtonCadastro = styled.div`
-background: #D32E00;
-box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
-text-align:center;
-padding: 2px;
-font-size: 50px;
-display: flex;
-max-width: 200px;
-height: auto;
-padding: 10px 10px 10px 10px;
-justify-content: center;
-align-items: center;
-@media (max-width:1200px){
-  width: 100%;
-  height: auto;
-  font-size: 30px;
-  padding: 12px 12px 12px 12px;
- };
-`;
-export const Title2 = styled.div`
+  color: #fff;
   white-space: nowrap;
-  padding: 100px;
-  color: #FFF;
+  padding: 300px;
   font-family: Montserrat;
   font-size: 40px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  @media (max-width:1200px){
-  font-size: 25px;
-
- };
+  @media (max-width: 1200px) {
+    font-size: 40px;
+  }
+`;
+export const Firstpart = styled.div`
+  /* width: 50%; */
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  /* margin-right: 2%; */
+  flex-direction: column;
+  @media (max-width: 1500px) {
+  }
+`;
+export const SecondPart = styled.div`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+export const ButtonCadastro = styled.div`
+  white-space: nowrap;
+  background: #d32e00;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
+  text-align: center;
+  padding: 2px;
+  font-size: 50px;
+  display: flex;
+  max-width: 300px;
+  height: auto;
+  padding: 8px 8px 8px 8px;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 1200px) {
+    width: 100%;
+    height: auto;
+    font-size: 40px;
+    padding: 10px 10px 10px 10px;
+  }
+`;
+export const Title2 = styled.div`
+  white-space: nowrap;
+  padding: 300px;
+  color: #fff;
+  font-family: Montserrat;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  @media (max-width: 1200px) {
+    font-size: 40px;
+  }
 `;
