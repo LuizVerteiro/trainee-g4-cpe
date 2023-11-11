fe64d445fad138958514f8310814327b0bdeadf5
Luiz Verteiro <luizverteiro1819@gmail.com>
2023-11-10 22:45:01 -0300

    Revert "Merge branch 'luiz-branch' into main"
    This reverts commit 00cb164df0b11ec2b5dd3301da94a1eca8b571e9, reversing
    changes made to 64eb6e7abeabd10064f8f854b9644b3bfc059a40.

--- trainee-cpe/src/Components/Header/Styles.js
+++ trainee-cpe/src/Components/Header/Styles.js
@@ -1,57 +1,19 @@
 import styled from "styled-components";
+
 export const DivBackground = styled.div`
-    position: fixed;
-    top:0;
-    left:0;
-
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    width: 100vw;
-    height: 11rem;
+    width: 100%;
+    height: 345px;
     background: linear-gradient(to bottom, #DE3100, black);
 `
-export const DivContainner = styled.div`
-    display: flex;
-    align-items: center;
-    padding-left: 1rem;
-`
 export const DivLogo = styled.div`
-    width: 5rem;
-    height: 5rem;
-    transition: transform 0.2s ease-in-out;
-    &:hover {
-        transform: scale(1.1); 
-    }
+    width: 208px;
+    height: 146px;
+    padding-left: 28%;
+    padding-top: 1%;
+    
 `
 export const DivText = styled.div`
-    font-size: 2rem;
-    font-weight: bold;
-`
-export const DivBottons = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 1rem;
-    flex-wrap: wrap;
-    padding-right: 1rem;
-`
-export const DivEntrar = styled.div`
-    border: 0px;
-    padding: 1rem;
-    transition: transform 0.2s ease-in-out;
-    &:hover {
-        transform: scale(1.1); 
-    }
-`
-export const DivCadastro = styled.div`
-    border: 1px solid white;
-    padding: 1rem;
-    border-radius: 10px;
-    margin-left: 2rem;
-    transition: transform 0.2s ease-in-out;
-    &:hover {
-        transform: scale(1.1); 
-    }
+    font-family: 'Monteserrat';
+    font-size: 60px;
+
 `
